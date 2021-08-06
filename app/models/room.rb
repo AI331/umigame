19385809fd425bdb3fb5391cb0d89c515bc017db
AI331 <akira0331ai@gmail.com>
2021-08-06 13:08:21 +0900

    roomモデルにnameの一意性制約のバリデーション追記

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
   has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false, message: 'そのルーム名は使用できません' }
 end
