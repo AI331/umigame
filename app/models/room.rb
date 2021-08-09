ed631cc61f60e0f05a484f5f6ec646afaa662a16
AI331 <akira0331ai@gmail.com>
2021-08-09 12:28:41 +0900

    messageモデル作成、アソシエーション追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
   has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users
-  validates :name, presence: true, uniqueness: { case_sensitive: false, message: 'そのルーム名は使用できません' }
+  validates :name, presence: true, uniqueness: { message: 'そのルーム名は使用できません' }
+  has_many :messages
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'rooms#index'
-  resources :rooms
   resources :users, only: [:edit, :update]
+  resources :rooms do
+    resources :messages, only: [:create]
+  end
+ 
 end
