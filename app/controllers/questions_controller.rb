7e34fc8acd52ffb7f771ec63b08bd653db20da44
AI331 <akira0331ai@gmail.com>
2021-08-19 23:07:01 +0900

    非同期通信で出題できるように設定

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,8 +2,10 @@ class QuestionsController < ApplicationController
   def create
     @room = Room.find(params[:room_id])
     quiz = Quiz.find(question_params[:quiz_id])
-    @question = @room.questions.create(title: quiz.title, q_content: quiz.quizzes_content, q_answer: quiz.answer, quiz_id: quiz.id)
-    redirect_to room_url(@room)
+    @question = @room.questions.new(title: quiz.title, q_content: quiz.quizzes_content, q_answer: quiz.answer, quiz_id: quiz.id)
+    if @question.save
+      ActionCable.server.broadcast 'question_channel', content: @question
+    end
   end
 
   private
